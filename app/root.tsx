d29084e8a4bca5ffc4df56bfd6063c9245bd6268
XiaoHeiZhi <2729173112@qq.com>
2023-12-05 16:03:31 +0800

    test

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { Meta, Outlet } from "@remix-run/react";
+import { Meta, Outlet, Scripts, Links } from "@remix-run/react";
 
 export default function App() {
   return (
@@ -7,9 +7,11 @@ export default function App() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+        <Links />
       </head>
       <body>
         <Outlet />
+        <Scripts />
       </body>
     </html>
   );
